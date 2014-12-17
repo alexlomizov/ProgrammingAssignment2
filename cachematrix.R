89b9b685db8d0a625d94da8425c5d65565cdfbcd
Alex Lomizov <a.lomizov@gmail.com>
2014-12-17 23:28:01 +0000

    Initial functions w/o comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,26 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(cachedMatrix = matrix()) {
+    CachedInv <- NULL
+    get <- function() cachedMatrix
+    setInv <- function(Inv) CachedInv <<- Inv
+    getInv <- function() CachedInv
+    list(get = get,
+         setInv = setInv,
+         getInv = getInv)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+    x.Inv <- x$getInv()
+    if(!is.null(x.Inv)) {
+        message("getting cached data")
+        return(x.Inv)
+    }
+    x.Inv <- solve(x$get())
+    x$setInv(x.Inv)
+    x.Inv
 }
